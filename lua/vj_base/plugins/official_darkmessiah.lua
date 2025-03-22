2af0c7c4ab4068dd873143f48f4d16f4d2a4ea02
DrVrej <DrVrej@users.noreply.github.com>
2025-03-22 03:57:59 -0400

    Update menu to a better version

--- lua/vj_base/plugins/official_darkmessiah.lua
+++ lua/vj_base/plugins/official_darkmessiah.lua
@@ -20,21 +20,21 @@ VJ.AddConVar("vj_dm_babyspawnlimit", 9, FCVAR_ARCHIVE)
 -- Menu --
 if CLIENT then
 	hook.Add("PopulateToolMenu", "VJ_ADDTOMENU_DM", function()
-		spawnmenu.AddToolMenuOption("DrVrej", "SNPC Configures", "Dark Messiah", "Dark Messiah", "", "", function(Panel)
+		spawnmenu.AddToolMenuOption("DrVrej", "SNPC Configures", "Dark Messiah", "Dark Messiah", "", "", function(panel)
 			if !game.SinglePlayer() && !LocalPlayer():IsAdmin() then
-				Panel:AddControl("Label", {Text = "#vjbase.menu.general.admin.not"})
-				Panel:AddControl( "Label", {Text = "#vjbase.menu.general.admin.only"})
+				panel:Help("#vjbase.menu.general.admin.not")
+				panel:Help("#vjbase.menu.general.admin.only")
 				return
 			end
-			Panel:AddControl( "Label", {Text = "#vjbase.menu.general.admin.only"})
-			Panel:AddControl("Button", {Text = "#vjbase.menu.general.reset.everything", Command = "vj_dm_nobabyspawn 1\n vj_dm_nextbaby 40\n vj_dm_babyspawnlimit 9"})
-			Panel:AddControl("Checkbox", {Label = "Spider Queen Spawns Baby Spiders", Command = "vj_dm_nobabyspawn"})
-			Panel:AddControl("Slider", {Label = "Baby Spider Limit", min = 3, max = 99, Command = "vj_dm_babyspawnlimit"})
-			Panel:ControlHelp("How many baby spiders are allowed per queen")
-			Panel:ControlHelp("Rounded to the nearest OR lowest multiple of 3!")
-			Panel:AddControl("Slider", {Label = "Next Baby Spider Spawn", min = 0, max = 300, Command = "vj_dm_nextbaby"})
-			Panel:ControlHelp("How much time until Spider Queen can spawn Spiders again?")
-			Panel:ControlHelp("Total of 300 seconds (5 Minutes)")
+			panel:Help("#vjbase.menu.general.admin.only")
+			panel:AddControl("Button", {Text = "#vjbase.menu.general.reset.everything", Command = "vj_dm_nobabyspawn 1\n vj_dm_nextbaby 40\n vj_dm_babyspawnlimit 9"})
+			panel:CheckBox("Spider Queen Spawns Baby Spiders", "vj_dm_nobabyspawn")
+			panel:NumSlider("Baby Spider Limit", "vj_dm_babyspawnlimit", 3, 99, 0)
+			panel:ControlHelp("How many baby spiders are allowed per queen")
+			panel:ControlHelp("Rounded to the nearest OR lowest multiple of 3!")
+			panel:NumSlider("Next Baby Spider Spawn", "vj_dm_nextbaby", 0, 300, 0)
+			panel:ControlHelp("How much time until Spider Queen can spawn Spiders again?")
+			panel:ControlHelp("Total of 300 seconds (5 Minutes)")
 		end)
 	end)
 end
